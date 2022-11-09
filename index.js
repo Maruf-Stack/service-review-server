d9f029db69a59193ec347cbc77b95a06c4af668b
Maruf <01780527702maruf@gmail.com>
2022-11-09 12:49:49 +0600

    connect database

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+//db : service-review
+//pass : 2zuaqKay7Z5kG4Yg
 
 app.get('/', (req, res) => {
     res.send('server is running')
 })
+
+
+
+const uri = "mongodb+srv://<username>:<password>@cluster0.ei8vvcb.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
+
 
 app.listen(port, () => {
     console.log(`server is running on port ${port}`)
