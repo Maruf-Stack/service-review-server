04bb6df65711bdbbcc3309a0dd191e1d0b6ea9f4
Maruf <01780527702maruf@gmail.com>
2022-11-11 12:22:55 +0600

    final commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors')
 app.use(cors())
 
@@ -32,6 +32,13 @@ async function run() {
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray()
             res.send(services)
+        })
+        app.get('/allservices/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const details = await database.findOne(query);
+            res.send(details)
+
         })
     }
     finally {
